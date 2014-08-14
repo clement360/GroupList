83e8970851f4a93836367c9fefed0299c817e8e3
Miguel Clement <miguelfclement@gmail.com>
2014-08-14 00:24:13 -0500

    changed port again (stilll running)

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
 
+var port = Number(process.env.PORT || 3000);
 console.log("Started");
 
 var users = [];
@@ -123,8 +124,8 @@ io.sockets.on('connection', function (socket) {
     
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+http.listen(port, function () {
+    console.log('listening on *' + port);
 });
 
 function userIndex(id) {
